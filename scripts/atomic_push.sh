df7a2493ac32b0ea691bffdd99b57d94e3384c59
Jordi Solà <jordi.sola@esperantotech.com>
2019-04-21 20:56:40 +0200

    Solving infinite loop

--- scripts/atomic_push.sh
+++ scripts/atomic_push.sh
@@ -1,8 +1,40 @@
+#!/bin/sh
 REPOROOT=$(git rev-parse --show-toplevel)
 
-git add $REPOROOT/logs
 
-until git push origin HEAD:master
-do
+# Credit: https://gist.github.com/willprice/e07efd73fb7f13f917ea
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_files() {
+  git checkout master
+  # Current month and year, e.g: Apr 2018
+  dateAndMonth=`date "+%b %Y"`
+  # Stage the modified files in dist/output
+  git add $REPOROOT/logs
+  # Create a new commit with a custom build message
+  # with "[skip ci]" to avoid a build loop
+  # and Travis build number for reference
+  git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+}
+
+upload_files() {
+  # Remove existing "origin"
+  git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://jsmont:${GH_TOKEN}@github.com/jsmont/data_prefetcher.git > /dev/null 2>&1
+
+  until git push origin master --quiet
+  do
     git pull
-done
+  done
+}
+
+setup_git
+
+commit_files
+
+upload_files
