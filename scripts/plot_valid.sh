7e2e96a5d970c8c438064b7174b51762b5013f72
Jordi Solà <jordi.sola@esperantotech.com>
2019-04-25 00:57:03 +0200

    Adding histogram

--- scripts/plot_valid.sh
+++ scripts/plot_valid.sh
@@ -12,7 +12,7 @@ if [ ! -z "$GEN_GH" ]; then
     echo "<html>
 <head>
 <title>Data prefetcher stats</title>
-<style>img{ width: 49%;} </style>
+<style>img{ width: 32%;} </style>
 </head>
 <body>" > $GH_FILE
 fi
@@ -25,9 +25,11 @@ for trace in $LOG_FOLDER/*; do
         tr=$(basename -- "$trace")
         tr=${tr%.*}
 
-        echo -e "Instructions\t$(basename $sim)\t$(basename $sim)" > $sim.csv
-        grep "Instructions Retired" $simulator >> $sim.csv
-        sed -i "s/.*Retired: \([0-9]\+\) .*IPC: \([0-9.]\+\) .*IPC: \([0-9.]\+\).*/\1\t\2\t\3/g" $sim.csv
+        echo -e "Instructions\t$(basename $sim)\t$(basename $sim)" > ${sim}_temporal.csv
+        grep "Instructions Retired" $simulator >> ${sim}_temporal.csv
+        sed -i "s/.*Retired: \([0-9]\+\) .*IPC: \([0-9.]\+\) .*IPC: \([0-9.]\+\).*/\1\t\2\t\3/g" ${sim}_temporal.csv
+        
+        grep "Simulation complete." $simulator | sed "s/.*Cycles elapsed: \([0-9.]\+\).*/$sim\t\1/g" >> ${tr}_aggregated.csv
     done
     if [ -z "$GEN_GH" ]; then
     echo "
@@ -36,7 +38,7 @@ for trace in $LOG_FOLDER/*; do
         set format x \"%.0f\"
         set ylabel \"IPC\"
         set key autotitle columnhead
-        FILES = system(\"ls -1 *.csv\")
+        FILES = system(\"ls -1 *_temporal.csv\")
         plot for [data in FILES] data using 1:2 with lines lw 2
     " | gnuplot --persist
     echo "
@@ -45,8 +47,18 @@ for trace in $LOG_FOLDER/*; do
         set format x \"%.0f\"
         set ylabel \"IPC\"
         set key autotitle columnhead
-        FILES = system(\"ls -1 *.csv\")
+        FILES = system(\"ls -1 *_temporal.csv\")
         plot for [data in FILES] data using 1:3 with lines lw 2
+    " | gnuplot --persist
+    echo "
+    set title \"$tr execution cycles\"
+    set style data histograms
+    set ylabel \"Cycles\"
+    set style fill solid 1.00 border lt -1
+    set xtics border in scale 1,0.5 nomirror rotate by -90 offset character 0, 0, 0
+    unset key
+    plot '${tr}_aggregated.csv' using 2:xticlabels(1) with histogram,\
+         \"\"  using 0:(\$2):(\$2) with labels notitle offset 2,1
     " | gnuplot --persist
     else
         echo "
@@ -57,7 +69,7 @@ for trace in $LOG_FOLDER/*; do
             set ylabel \"IPC\"
             set format x \"%.0f\"
             set key autotitle columnhead
-            FILES = system(\"ls -1 *.csv\")
+            FILES = system(\"ls -1 *_temporal.csv\")
             plot for [data in FILES] data using 1:2 with lines lw 2
         " | gnuplot
         echo "<img src=\"img/${tr}_temporal.png\" alt=\"Temporal IPC evolution for all prefetchers on $tr code.\">" >> $GH_FILE
@@ -69,10 +81,23 @@ for trace in $LOG_FOLDER/*; do
             set ylabel \"IPC\"
             set format x \"%.0f\"
             set key autotitle columnhead
-            FILES = system(\"ls -1 *.csv\")
+            FILES = system(\"ls -1 *_temporal.csv\")
             plot for [data in FILES] data using 1:3 with lines lw 2
         " | gnuplot
         echo "<img src=\"img/${tr}_accumulative.png\" alt=\"Temporal IPC evolution for all prefetchers on $tr code.\">" >> $GH_FILE
+        echo "
+            set term png
+            set output \"$REPOROOT/docs/img/${tr}_histogram.png\"
+            set title "$tr execution cycles"
+            set style data histograms
+            set ylabel "Cycles"
+            set style fill solid 1.00 border lt -1
+            set xtics border in scale 1,0.5 nomirror rotate by -90 offset character 0, 0, 0
+            unset key
+            plot '${tr}_aggregated.csv' using 2:xticlabels(1) with histogram,\
+                 \"\"  using 0:($2):($2) with labels notitle offset 2,1
+        " | gnuplot --persist
+        echo "<img src=\"img/${tr}_histogram\" alt=\"Temporal IPC evolution for all prefetchers on $tr code.\">" >> $GH_FILE
     fi
     rm *.csv
     cd $LOG_FOLDER
